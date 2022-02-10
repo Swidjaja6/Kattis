d0708c65d327a9c8bde1e94fa978a47cf6a677b3
Sean Widjaja <49921252+Swidjaja6@users.noreply.github.com>
2022-02-09 21:49:06 -0800

    Update and rename fart.java to Solution.java

--- Java/NarrowArtGallery/src/Solution.java
+++ Java/NarrowArtGallery/src/Solution.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class fart {
+public class Solution {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
